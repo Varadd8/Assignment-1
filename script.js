ee38641daba923f4721bc7fe4c000c427e69aef8
Varadd8 <153820668+Varadd8@users.noreply.github.com>
2024-07-08 23:02:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function result() 
+{
+				let score=0;
+				if (document.getElementById('correct1').checked)
+				{
+					score++;
+				}
+				if (document.getElementById('correct2').checked)
+				{
+					score++;
+				}
+				if (document.getElementById('correct3').checked)
+				{
+					score++;
+				}
+				if (document.getElementById('correct4').checked)
+				{
+					score++;
+				}
+				alert("Your score is :" + score)
+			}
